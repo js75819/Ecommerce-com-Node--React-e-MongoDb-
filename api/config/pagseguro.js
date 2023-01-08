16679520834ae72f2d2e35e917b04d9535a6dd88
João Bosco Santos <40505660+js75819@users.noreply.github.com>
2023-01-08 05:21:01 -0300

    Delete pagseguro.js

--- api/config/pagseguro.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    mode: process.env.NODE_ENV === 'production' ? "live" : 'sandbox',
-    sandbox: process.env.NODE_ENV === 'production' ? false : true,
-    sandbox_email: process.env.NODE_ENV === 'production' ? null : 'c43452522542774085026@sandbox.pagseguro.com.br',
-    email: 'js75819@gmail.com',
-    token: '8FE09660563C428C8A360EDF766859DD',
-    notificationURL: "https://api.loja-teste.ampliee.com/v1/api/pagamentos/notificacao",
-}
